de02530e4e1b2c9b07820de169f364a904349698
Will Fay <williamfay08@gmail.com>
2023-02-17 14:26:24 -0500

    Updates to Projects page

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,20 @@
+import '../styles/module.projects.css';
+import jeopardyImg from '../assets/jeopardy_screenshot.png';
 
 export default function Projects() {
     return (
-        <h2>Projects</h2>
+        <>
+            <div class='projects_header_container'>
+                <h1 class='header_text'>My Projects</h1>
+                <h2 class='header_info'>*All projects are considered works in progress and are subject to change*</h2>
+            </div>
+            <div class='projectOne_container'>
+                <img src={jeopardyImg} alt='Image of jeopardy project' />
+                <div class='projectOne_header_container'>
+                    <h1>Jeopardy Project</h1>
+                    <h2>About</h2>
+                </div>
+            </div>
+        </>
     )
 }
